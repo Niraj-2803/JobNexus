5d119984d1fcd69d96394d3b96e2a0cb816aa6b2
Niraj-2803 <nirajtupsundar28@gmail.com>
2024-11-06 13:09:18 +0530

    Added MongoDB

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 
 app.use('/', jobRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
