e34d4fe00a69d9e2958a75651c7fa84cc483a98f
Niraj-2803 <nirajtupsundar28@gmail.com>
2024-11-06 13:24:55 +0530

    Added Frontend

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://jobnexus-4x2g.onrender.com';
 
 export const fetchJobs = async (jobType = '', location = '') => {
   try {
